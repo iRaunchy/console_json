ab117cd3d211e143ff4915cb8b83b93ce0f11c78
Pavlo Chobot <iraunchy@gmail.com>
2018-07-26 19:33:54 +0300

    added unit test

--- src/test/java/com/techtask/console_json/ConsoleJsonApplicationTests.java
+++ src/test/java/com/techtask/console_json/ConsoleJsonApplicationTests.java
@@ -1,7 +1,9 @@
 package com.techtask.console_json;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -9,8 +11,17 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class ConsoleJsonApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    @Autowired
+    CommandLineJSONProcessor commandLineJSONProcessor;
+
+    private static final String filePath = "./src/test/resources/test_file/test_json.json";
+
+    @Test
+    public void verifyJSONProcessorTest() {
+        String expectedResult = "*** Found 3 objects with field childCount equals 2 **** \n" +
+                "*** Found 5 childCount ***";
+        String actualResult = commandLineJSONProcessor.processJSONWithParameters(filePath, "childCount", "2");
+        Assert.assertEquals("The result of processing JSON is incorrect",expectedResult, actualResult);
+    }
 
 }
